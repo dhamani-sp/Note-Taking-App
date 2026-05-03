9dadd5d97c4863d02e37659e0a59712fc2531130
dhamani-sp <spdhamani.23@gmail.com>
2026-05-03 15:08:51 +0530

    initial implementation

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile("pages/index.html", {root: __dirname})
+})
+app.get('/login', (req, res) => {
+  res.sendFile("pages/login.html", {root: __dirname})
+})
+app.get('/signup', (req, res) => {
+  res.sendFile("pages/signup.html", {root: __dirname})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
